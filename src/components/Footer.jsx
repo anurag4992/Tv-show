a5837cc11706e896faa7b0bc87080b88d9ea0cd0
Anurag <anuragvastav4992@gmail.com>
2022-03-06 03:29:29 +0530

    new branch commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import "../styles.css";
+
+function Footer(){
+
+    const dat=new Date().getFullYear();
+    return <div className="footer">
+        <h4 className="h4Footer">Made by Anurag</h4>
+        <p className="pFooter">Copyright @{dat}</p>
+    </div>;
+}
+
+export default Footer;
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import Card from "./Card";
 import "../styles.css";
 import { useLocation, Link, } from "react-router-dom";
@@ -7,72 +8,51 @@ import { useLocation, Link, } from "react-router-dom";
 
 export default function Summary() {
 
-  const {obj, setObj}=useState({
-    fname : "",
-    lname: "",
-    email: "",
-    list: ""
-  });
- 
-
- function handleChange(event){
-   const {name, value}=event.target;
-   setObj((prevValue) => {
-    return {...prevValue,
-    [name]: value
-    }
-   })
-  }
-
-
-  function handleSubmit(e){
-    e.preventDefault();
-    localStorage.setItem(fname, obj);
-  }
 
   const location = useLocation();
   const state = location.state;
 
   return <div>
     <Header />
-    <div className="container-fluid">
-      <div className="container">
+    <div className="container-fluid  summary-body">
+      <div className="topic">
         <h1 className="summaryText">{state.name}</h1>
-        <Link className="btn btn-primary text" to="/">Home</Link>
-        <p className="summary">{state.summary}</p>
+        <p className="summary">Imdb : {state.imdb}</p>
       </div>
-      <div>
-        <img src={state.img} className="card-img-top img" alt="Img" />
-        <p className="summary">{state.imdb}</p>
-      </div>
-    </div>
-    <div className="container">
-      <a className="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-        Book movie ticket
+        <Link className="btn btn-primary home-btn" to="/">Home</Link>
+      
+      <a className="btn btn-primary right" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+        Book movie ticket<a href="#fname" style={{display: "none"}}></a>
       </a>
+      <p className="summary">{state.summary}</p>
+      <div className="imgForm">
+        <img src={state.img} className="card-img-top img" alt="Img" />
+        
+        <div className="forms">
+      
       <div className="collapse" id="collapseExample">
         <div className="card card-body">
 
-          <form onSubmit={handleSubmit}>
+          <form>
 
             <h2 className="mb-3">Enter details</h2>
             <div className="form-floating mb-3">
-              <input type="text" onChange={handleChange} value={obj.fname} name="fname" className="form-control" id="fname" placeholder="Enter first name .." autoComplete="off" required/>
+              <input type="text" className="form-control" id="fname" placeholder="Enter first name .." autoComplete="off" required/>
               <label htmlFor="floatingInput">Your first name</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input type="text" onChange={handleChange} value={obj.lname} name="lname" className="form-control"  id="lname" placeholder="Enter last name .." autoComplete="off" required/>
+              <input type="text" className="form-control"  id="lname" placeholder="Enter last name .." autoComplete="off" required/>
               <label htmlFor="floatingInput">Your last name</label>
             </div>
 
             <div className="form-floating mb-3">
-              <input type="email" onChange={handleChange} value={obj.email} name="email" className="form-control"  id="email" placeholder="eg. name@example.com" autoComplete="off" required/>
+              <input type="email" className="form-control"  id="email" placeholder="eg. name@example.com" autoComplete="off" required/>
               <label htmlFor="floatingInput">Email address</label>
             </div>
 
             <label htmlFor="list" className="form-label">Choose movie</label>
-            <input className="form-control" onChange={handleChange} value={obj.list} name="list" list="datalistOptions"  id="list" placeholder="Type to search..." required/>
+            <input className="form-control" list="datalistOptions"  id="list" placeholder="Type to search..." required/>
             <datalist id="datalistOptions">
               <option value="Doomsday" />
               <option value="2012" />
@@ -86,8 +66,11 @@ export default function Summary() {
 
         </div>
       </div>
+      </div>
     </div>
-
+    
+    </div>
+    <Footer />
   </div>;
 
 }
